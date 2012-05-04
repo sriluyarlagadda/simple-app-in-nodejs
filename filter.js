b0cbc5168fae4645b57ff0eeb31939abbaa5af12
Srilu <sri_hariy@yahoo.com>
2012-05-04 23:35:52 +0800

    new function

--- filter.js
+++ filter.js
@@ -14,3 +14,20 @@ var objects = [{a:'a',b:'b',c:'c'},{b:'2',c:'1'},{d:'3',e:'4'},{e:'f',c:'c'}];
 objects.forEach(function(object){
 	console.log('Result',match(object,{c:'c',d:'3'}));
 });
+
+
+console.log( doSomething() );
+
+// define the functions after calling them!
+var doSomethingElse = function() { return 'doSomethingElse'; };
+
+function doSomething() { return 'doSomething'; }
+
+console.log( doSomethingElse() );
+
+
+var doSomethingElse = function(a, b) {
+  console.log(a, b);
+  console.log(arguments);
+};
+doSomethingElse(1, 2, 3, 'foo');
